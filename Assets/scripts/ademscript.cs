a4ff4b6eca0db95d10249dba8dd6fdef6d7c5ed1
jochem33 <jochemjuriaan.baas@coderclass.nl>
2018-03-26 11:05:42 +0200

    [change] ademscript werkend gemaakt met A en D key, werkt nog niet met on-screen buttons

--- Assets/scripts/ademscript.cs
+++ Assets/scripts/ademscript.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ademscript : MonoBehaviour {
-	bool ademinbool_player;
-	bool ademuitbool_player;
+	public bool ademinbool_player;
+	public bool ademuitbool_player;
 	public int ademgetal = 100;
 	public int nietgeademgetal = 100;
 
@@ -15,18 +15,32 @@ public class ademscript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		ademinbool_player = GameObject.Find ("adem in").GetComponent<ademin> ().ademinbool;
-		ademuitbool_player = GameObject.Find ("adem uit").GetComponent<ademuit> ().ademuitbool;
+		//ademinbool_player = GameObject.Find ("adem in").GetComponent<ademin> ().ademinbool;
+		//ademuitbool_player = GameObject.Find ("adem uit").GetComponent<ademuit> ().ademuitbool;
 	
+		if (Input.GetKey (KeyCode.A)) {
+			ademinbool_player = true;
+		} else {
+			ademinbool_player = false;
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			ademuitbool_player = true;
+		} else {
+			ademuitbool_player = false;
+		}
+
 		if (ademinbool_player == true) {
 			ademgetal += 1; 
 			nietgeademgetal = 100;
-		} else if (ademinbool_player == true) {
+		} else if (ademuitbool_player == true) {
 			ademgetal -= 1; 
 			nietgeademgetal = 100;
 		} else {
 			nietgeademgetal -= 1;
 		}
+
+		Debug.Log (ademgetal + " " + nietgeademgetal);
 	}
 
 	public void ademinstate(bool adembool2) {
