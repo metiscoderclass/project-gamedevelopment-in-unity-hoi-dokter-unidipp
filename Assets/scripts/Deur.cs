4fdaedc2515fe08f1b7fcf8c464204b71df70b52
Jurre de Vries <31882165+R4KE@users.noreply.github.com>
2018-03-27 09:42:28 +0200

    i9k hepp de filles tovegoetf 14sadh

---
+++ Assets/scripts/Deur.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+using UnityEngine;
+
+public class Deur : MonoBehaviour {
+
+    public Button Btndeur;
+
+    void Start()
+    {
+        Button btn = Btndeur.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("deur open!");
+    }
+}
