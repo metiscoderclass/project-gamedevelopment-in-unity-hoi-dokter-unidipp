c0fd38c84e9ee397aa3dd41e111dfe41d9d4b51f
jochem33 <jochemjuriaan.baas@coderclass.nl>
2018-04-02 11:18:44 +0200

    [add] message verdwijnt weer [change] adem animatie werkt beter maar repeat nog

--- Assets/scripts/ademscript.cs
+++ Assets/scripts/ademscript.cs
@@ -48,32 +48,36 @@ public class ademscript : MonoBehaviour {
 
 		if (Input.GetKey (KeyCode.A)) {
 			ademinbool_player = true;
+			animator.SetBool("ademIn", true);
 		} else {
 			ademinbool_player = false;
+			animator.SetBool("ademIn", false);
 		}
 
 
 
 		if (Input.GetKey (KeyCode.D)) {
 			ademuitbool_player = true;
+			animator.SetBool("ademUit", true);
 		} else {
 			ademuitbool_player = false;
+			animator.SetBool("ademUit", false);
 		}
 
 
 
 		if (ademinbool_player == true) {
+			
 			if (eersteadem == false) {
 				eersteadem = true;
 			}
-				
-			animator.SetBool("ademIn", true);
+
+			Destroy (GameObject.FindGameObjectWithTag("message"));
+
 
 			ademgetal += 1; 
 			nietgeademgetal = 100;
 		} else if (ademuitbool_player == true) {
-
-			animator.SetBool("ademUit", true);
 
 			ademgetal -= 1; 
 			nietgeademgetal = 100;
