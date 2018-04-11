a042fd9ebeff42e70caf2aa029291ae6712e2181
jochem33 <jochemjuriaan.baas@coderclass.nl>
2018-04-11 12:41:08 +0200

    [add] script van internet om te proberen of ingedrukt houden werkt

---
+++ Assets/scripts/keyinput1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class keyinput1 : MonoBehaviour {
+
+	public string inputName;
+	Button buttonMe;
+	// Use this for initialization
+	void Start () {
+		buttonMe = GetComponent<Button>();
+	}
+
+	void Update() {
+		if(Input.GetButtonDown(inputName))
+		{
+			buttonMe.onClick.Invoke();
+		}
+
+
+	}
+}
